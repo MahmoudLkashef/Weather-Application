af0a40e97b91c8ef2fb0e1d7f34fe0ad643604e2
mahmoudLkashef <mahmoud.reda.e12@gmail.com>
2022-11-06 19:13:17 +0200

    Added data binding to details fragment

--- app/src/main/java/com/example/weatherapp/ui/DetailsFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/DetailsFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+
+    lateinit var binding :FragmentDetailsBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_details,
+            container,
+            false
+        )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

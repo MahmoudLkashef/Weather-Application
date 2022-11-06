5d39a9cc17e4526cd32d27f6591660b2c8f5c81f
mahmoudLkashef <mahmoud.reda.e12@gmail.com>
2022-11-06 17:43:44 +0200

    Added response model

---
+++ app/src/main/java/com/example/weatherapp/model/Response.kt
@@ -0,0 +1,39 @@
+package com.example.weatherapp.model
+
+import android.os.Parcelable
+import com.example.weatherapp.utils.WeatherUtil
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Response(var list: List<WeatherData>,var city:City): Parcelable
+
+@Parcelize
+data class City(var name:String,var coord:Coord,var country:String , var timezone:Int): Parcelable
+
+@Parcelize
+data class Coord(var lat:Double,var lon:Double): Parcelable
+
+@Parcelize
+data class WeatherData(val dt:Int,val main: Main,val weather:List<Weather>,val wind:Wind
+,val dt_txt:String) : Parcelable
+{
+    val dt_txt_date:String
+    get()=dt_txt.split(" ").get(0)
+
+    val dt_txt_day:String
+    get() = WeatherUtil.convertDateToDay(dt_txt.split(" ").get(0))
+}
+
+@Parcelize
+data class Main(val temp:Double ,val pressure:Double,val humidity:Double): Parcelable
+{
+    val tempString:String
+    get() = temp.toString()+"°C"
+}
+
+@Parcelize
+data class Weather(val id:Int,val main:String,val description:String,val icon:String): Parcelable
+
+@Parcelize
+data class Wind(val speed:Double,val deg:Double,val gust:Double): Parcelable
+
---
+++ app/src/main/java/com/example/weatherapp/utils/WeatherUtil.kt
@@ -0,0 +1,52 @@
+package com.example.weatherapp.utils
+
+import com.example.weatherapp.model.WeatherData
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class WeatherUtil {
+    companion object{
+        fun getCurrentDayWeather(list: List<WeatherData>,date:String):List<WeatherData>
+        {
+            var currentDayList=ArrayList<WeatherData>()
+            for (weatherData in list)
+            {
+                val weatherDataDate=weatherData.dt_txt.split(" ").get(0)
+                if(weatherDataDate.equals(date))currentDayList.add(weatherData)
+            }
+            return currentDayList
+        }
+
+        fun getNextFiveDaysWeather(list: List<WeatherData>):List<WeatherData>
+        {
+            val currentDate=list.get(0).dt_txt.split(" ").get(0)
+            var map= mutableMapOf<String, WeatherData>()
+            for(i in list.indices)
+            {
+                var date=list.get(i).dt_txt.split(" ").get(0)
+                if(!map.containsKey(date)) map.put(date,list.get(i))
+            }
+            map.remove(currentDate)
+            return map.values.toList()
+        }
+
+        fun convertDateToDay(date:String):String
+        {
+            val firstDate = SimpleDateFormat("dd-MM-yyyy").parse(date)
+            val day= SimpleDateFormat("EEEE").format(firstDate)
+            return day
+        }
+
+        private fun getCurrentDateTime(): Date {
+            return Calendar.getInstance().time
+        }
+
+        fun getCurrentTime():String
+        {
+            return SimpleDateFormat("hh:mm").format(getCurrentDateTime()).toString()
+
+        }
+    }
+
+}

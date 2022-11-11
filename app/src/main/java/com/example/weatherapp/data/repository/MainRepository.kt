ee4667f8860046a311add0ac99b15865c457ab3b
mahmoudLkashef <mahmoud.reda.e12@gmail.com>
2022-11-11 13:27:59 +0200

    Fixed home data

--- app/src/main/java/com/example/weatherapp/data/repository/MainRepository.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/MainRepository.kt
@@ -16,6 +16,7 @@ class MainRepository @Inject constructor(
     suspend fun getDataFromApi(city: String) {
         val response = remoteRepository.getWeatherData(city)
         val responseList = WeatherMappers.convertResponseToWeatherResponse(response!!)
+        localRepository.deleteAll()
         localRepository.insertWeatherResponseList(responseList)
     }
 
